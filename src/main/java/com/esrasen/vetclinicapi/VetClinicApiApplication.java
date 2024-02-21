82d7e12a59ff41f559f901f6a8894e5fb48f9dac
Esra Şen <sen.esra@yahoo.com>
2024-02-21 17:03:52 +0300

    Initial commit

---
+++ src/main/java/com/esrasen/vetclinicapi/VetClinicApiApplication.java
@@ -0,0 +1,13 @@
+package com.esrasen.vetclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetClinicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esrasen/vetclinicapi/VetClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esrasen.vetclinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
